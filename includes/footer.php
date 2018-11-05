911500b8a41700207353b533670d3297a8e4df60
Wateen Afzal <m6.93wateen@gmail.com>
2018-11-05 10:57:27 +0500

    fix conflict between commits

--- includes/footer.php
+++ includes/footer.php
@@ -4,11 +4,7 @@
 </footer>
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.js"></script>
-<<<<<<< HEAD
-    
     <script src="assets/js/bootstrap-notify.js"></script>
-=======
->>>>>>> parent of 3a7b291... include notiyf and animate
     <script src="assets/js/script.js"></script>
     </body>
 </html>
